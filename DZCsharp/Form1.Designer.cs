0c28ef0e621973deccfdbb510cfbdb6a212658fe
Way1ander <arsl2@rambler.ru>
2015-12-17 16:48:33 +0300

    version 1.01

---
+++ DZCsharp/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace DZCsharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.94764F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(217, 871);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 44);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.94764F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(217, 813);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 44);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.94764F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(216, 925);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 44);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(204, 54);
+            this.listView1.Margin = new System.Windows.Forms.Padding(6);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1227, 727);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Process Name";
+            this.columnHeader1.Width = 129;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Process ID";
+            this.columnHeader2.Width = 65;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Start Time";
+            this.columnHeader3.Width = 113;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Life Time";
+            this.columnHeader4.Width = 121;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Total Foreground Time";
+            this.columnHeader5.Width = 132;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.94764F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(1440, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 44);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.94764F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(217, 980);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(122, 44);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "label5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1933, 1224);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ DZCsharp/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace DZCsharp
+{
+    public partial class Form1 : Form
+    {
+        Int32 j = 0;
+        StringBuilder sb;
+        StreamWriter sw;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Process[] processes;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer2.Start();
+            timer1.Start();
+            sw = new StreamWriter(@"F:\DZCsharp\DZCsharp\X.txt");
+        }
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern UInt32 GetWindowThreadProcessId(Int32 hWnd, out Int32 lpdwProcessId);
+        [System.Runtime.InteropServices.DllImport( "user32.dll", SetLastError = true )]
+        public static extern Int32 GetForegroundWindow();
+        int ms,min,sec,sum, sum0=0;
+  
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+           /* DateTime one = DateTime.Now;
+            DateTime two = one + Convert.ToDatetime
+            TimeSpan result = one+100;
+            MessageBox.Show(result.Seconds.ToString());*/
+
+            Int32 k = GetForegroundWindow(); //Дескриптор окна в форграунде
+            if (j == k)
+            {
+                ms += 1;
+                if (ms == 100) { ms = 0; sec += 1; }
+                if (sec == 60) { sec = 0; min += 1; }
+                label1.Text ="Current process in foreground time " +min + ":" + sec + ":" + ms;
+                sum = min * 60000 + sec * 1000 + ms;
+                if (sum > sum0)
+                {
+                    sum0 = sum;
+                    Int32 pId;
+                    GetWindowThreadProcessId(k, out pId);
+                    label5.Text ="The longest time in foreground. Process: " +
+                        Convert.ToString(Process.GetProcessById(pId)) + " Time: "
+                        + min +":"+sec+":"+ms  ;
+                }
+            }
+            else
+            {
+                ms = 0; sec = 0; min = 0;
+                sb = new StringBuilder();
+
+                sb.Append(string.Format("{0}\t\n", label2.Text));
+                sb.Append(string.Format("{0}\t\n", label3.Text));
+                sb.Append(string.Format("{0}\t\n", label1.Text));
+                sw.WriteLine(sb.ToString());
+
+            }
+            j = k;
+            Int32 pID;
+            GetWindowThreadProcessId(k, out pID); // ID процесса, который создал окно в foreground
+            String lv7 = Convert.ToString(pID);
+            String lv8 = Convert.ToString(Process.GetProcessById(pID)); //Название процесса в форграунде 
+            label2.Text = "The foreground window handle: " + Convert.ToString(k);
+            label3.Text = "ID and ProcessName in foreground: " + lv7 + " " + lv8;
+            timer2.Interval = 1;
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            listView1.Items.Clear();
+            Process[] proce = Process.GetProcesses();
+
+            processes = Process.GetProcesses();
+            foreach (Process instance in processes)
+            {
+                try
+                {
+                    String lv1 = instance.ProcessName;
+                    String lv2 = Convert.ToString(instance.Id);
+                    String lv3 = Convert.ToString(instance.StartTime);
+                    String lv4 = Convert.ToString(instance.TotalProcessorTime);
+                    ListViewItem item = new ListViewItem(new string[] { lv1, lv2, lv3, lv4 });
+                    listView1.Items.Add(item);
+
+                }
+                catch (Exception ex)
+                { }
+            }
+            label4.Text = "Number of processes : " + proce.Length;
+            //Сохранение
+            if (listView1.Items.Count > 0)
+            {
+
+
+                foreach (ListViewItem lvi in listView1.Items)
+                {
+                    sb = new StringBuilder();
+
+                    foreach (ListViewItem.ListViewSubItem listViewSubItem in lvi.SubItems)
+                    {
+                        sb.Append(string.Format("{0}\t", listViewSubItem.Text));
+                    }
+                    sw.WriteLine(sb.ToString());
+                }
+                sw.WriteLine();
+            }
+            timer1.Interval = 4000;
+        }
+    }
+
+    }
